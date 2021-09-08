20c7206723845aece3ecc023435cc0ceb89c0959
lucasx0 <lucas.almeidasilva49@gmail.com>
2021-09-08 10:56:04 -0300

    finalizando as classes de dominio

--- src/main/java/com/lucas/bookstore/domain/Categoria.java
+++ src/main/java/com/lucas/bookstore/domain/Categoria.java
@@ -1,13 +1,28 @@
 package com.lucas.bookstore.domain;
 
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Categoria {
+@Entity
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 
 	public Categoria() {
