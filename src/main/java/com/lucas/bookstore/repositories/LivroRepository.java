b9e076b5a379768794684386bf11d7c6be339f2e
lucasx0 <lucas.almeidasilva49@gmail.com>
2021-09-08 11:09:00 -0300

    repositories implementadas e começando o jpa

---
+++ src/main/java/com/lucas/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.lucas.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucas.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+	
+}
